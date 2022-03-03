03ad7fc2e3b4c7313721e668ae2b5b079b753d2d
Clockworks <100453437+cclockworkks@users.noreply.github.com>
2022-03-03 13:21:41 -0800

    Delete source directory

--- source/games.lua
+++
@@ -1,6 +0,0 @@
-local Supported = {
-    [] = "",
-    [] = "",
-    [] = ""
-}
-local ElysiumGame = Supported[game.PlaceId]
--- source/premium.lua
+++
@@ -1,8 +0,0 @@
--- DM Clockworks#2000 a well written paragraph on why you should get Elysium premium.
-
-local PremiumCheck = {
-    123,
-    123,
-    123
-}
-return PremiumCheck

b926bfc8a7d7ed40f0ffab397efd08e4dcf13e42
clocks <100453437+cclockworkks@users.noreply.github.com>
2022-03-12 14:49:46 -0800

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,20 +1,4 @@
-local gm = getrawmetatable(game)
-setreadonly(gm, false)
-local namecall = gm.__namecall
-gm.__namecall =
-    newcclosure(
-    function(self, ...)
-        local args = {...}
-        if not checkcaller() and getnamecallmethod() == "FireServer" and tostring(self) == "MainEvent" then
-            if tostring(getcallingscript()) ~= "Framework" then
-                return
-            end
-        end
-        if not checkcaller() and getnamecallmethod() == "Kick" then
-            return
-        end
-        return namecall(self, unpack(args))
-    end)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/cclockworkks/elysium/main/library/misc.lua"))()
 
 -- // Dependencies
 local Aiming = loadstring(game:HttpGet("https://raw.githubusercontent.com/cclockworkks/elysium/main/storage/dahoodsilentaim.lua"))()
